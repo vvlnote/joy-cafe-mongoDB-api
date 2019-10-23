577f8f68cdbb4573dcb05896e36170441fdf37e4
Vivvie Lin <vvlnote@hotmail.com>
2019-10-22 17:15:02 -0700

    update README file about how to run this program, and also in the get dishes to populate the ingredients inside each dishes

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # README
 
 ##How to run this program. 
+- first need to ivoke the mongodb, by typing mongo on the terminal
 - the express server is running on port 4000
 - the MongoDB is running on the local machine, which is mongodb://127.0.0.1:27017/cafedb.
 - in order to have the sample data in the Monogdb - cafedb, you can run node sampleData.js on your terminal to push the sample data into the monogdb
--- routes/dishes.js
+++ routes/dishes.js
@@ -3,9 +3,16 @@ let Dish = require('../models/dish.model');
 
 router.route('/').get((req, res) => {
 	Dish.find()
+		.populate('ingredients.ingredient')
 		.then(dishes => res.json(dishes))
 		.catch(err => res.status(400).json("Error: " + err));
 });
 
 
 module.exports = router;
+
+// Dish.find({})
+// 	.populate('ingredients.ingredient')
+// 	.exec(function(error, dishes) {
+// 		console.log(JSON.stringify(dishes, null, "\t"))
+// })
